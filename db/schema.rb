3086d707234472967f5f02ff878d36dee3f38a79
Federico Capaldo <federico.capaldo@hotmail.com>
2025-11-19 14:23:55 +0000

    added plants entity related to garden

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_11_19_132859) do
+ActiveRecord::Schema[7.1].define(version: 2025_11_19_142309) do
   create_table "gardens", force: :cascade do |t|
     t.string "name"
     t.string "banner_url"
@@ -18,4 +18,14 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_19_132859) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "plants", force: :cascade do |t|
+    t.integer "garden_id", null: false
+    t.string "name"
+    t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["garden_id"], name: "index_plants_on_garden_id"
+  end
+
+  add_foreign_key "plants", "gardens"
 end
